89cc74a3b1426d7a91c51cc916c6c3cf12ba5559
kscardinal <kschang@cardinalhs.net>
2025-09-12 10:43:42 -0400

    Working on the webapp

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Day Tracker Webapp | Sonnaz Group</title>
+    <link rel="icon" type="image/png" sizes="256x256" href="assets/favicon.png">
+    <link rel="stylesheet" href="scss/bootstrap.css">
+</head>
+<body style="background: #1B1C1E">
+<?php
+include 'header.html';
+include 'index.html';
+?>
+</body>
+</html>
